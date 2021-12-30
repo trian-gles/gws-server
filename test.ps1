7833d45c55abf6398a87b150b0471d959b3f662e
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-12-30 13:14:24 -0500

    creating new users

--- test.ps1
+++ test.ps1
@@ -4,3 +4,9 @@ Write-Output "Getting user fatcow"
 Invoke-WebRequest -Method Get -Uri "http://localhost:3000/users/fatcow"
 Write-Output "Getting nonexistent user fatcat"
 Invoke-WebRequest -Method Get -Uri "http://localhost:3000/users/fatcat"
+Write-Output "Making new user"
+$body = @{
+    'username'='crazypig'
+    "port"=1234
+   } | ConvertTo-Json
+Invoke-WebRequest -UseBasicParsing -Uri "http://localhost:3000/users" -ContentType "application/json" -Method POST -Body $body
--- users.js
+++ users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 // { username, ip, port, outgoing challenges, incoming challenges }
-var users = [{username: "fatcow", ip: "127.0.0.1", port: 8001, outChallenges: [], inChallenges:[]}];
+var users = [{username: "fatcow", ip: "127.0.0.1", port: 8001, isOnline: false, outChallenges: [], inChallenges:[]}];
 
 module.exports = router;
+
+
 router.get('/', (req, res) => res.json(users));
 router.get('/:username', (req, res) => {
     for (let i = 0; i < users.length; i++)
@@ -16,4 +18,30 @@ router.get('/:username', (req, res) => {
     res.status(404);
     res.json({message: "Not Found"});
     
+});
+
+
+router.post('/', (req, res) =>{
+    var username = req.body.username;
+
+    // first check if a user exists with this name
+    for (let i = 0; i < users.length; i++){
+        if (users[i].username == username){
+            res.status(400);
+            res.json({message: `Username ${username} is already taken`});
+            return;
+        }
+    }
+
+    users.push(
+        {
+            username: username,
+            ip: req.ip,
+            port: req.body.port,
+            isOnline: true,
+            outChallenges: [],
+            inChallenges: []
+        });
+
+    res.json({message: `New user ${username} created`});
 });
